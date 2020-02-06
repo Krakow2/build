2896f52b69b0c14c79372946692cdfdc0274ea4b
Krakow2 <50423999+Krakow2@users.noreply.github.com>
2020-02-07 01:57:09 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,14 @@ $(document).ready(function () {
     autoplaySpeed: 2000
   });
 });
+
+$('.menu-btn').on('click', function (e) {
+  e.preventDefault();
+  $('.menu').toggleClass('menu_active');
+  $('.content').toggleClass('content_active');
+});
+
+$('.menu-btn').on('click', function (e) {
+  e.preventDefault;
+  $(this).toggleClass('menu-btn_active');
+});
